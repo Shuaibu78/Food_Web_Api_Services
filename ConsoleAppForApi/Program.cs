499306684fa3ee67d7c0de03f16e3f52faef83d4
einatmor115 <44752190+einatmor115@users.noreply.github.com>
2019-10-15 13:54:27 +0300

    Add files via upload

---
+++ ConsoleAppForApi/Program.cs
@@ -0,0 +1,86 @@
+﻿using FoodEntityFW;
+using FoodEntityFW.Food_entityFramWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForApi
+{
+    class Program
+    {
+        public static HttpClient webClient = new HttpClient();
+        private const string URL = "http://localhost:50358/api/foody";
+
+        static void Main(string[] args)
+        {
+            webClient.BaseAddress = new Uri(URL);
+            webClient.DefaultRequestHeaders.Accept.Clear();
+            webClient.DefaultRequestHeaders.Accept.Add(
+           new MediaTypeWithQualityHeaderValue("application/json"));
+
+            AddRecordes();
+            ShowRecordes();
+        }
+
+        static async Task<Uri> CreateFoodAsync(Food msg, HttpClient client)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/foody", msg);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static public void ShowRecordes()
+        {
+            HttpResponseMessage httpResponseMessage = webClient.GetAsync("").Result;
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var dataObject = httpResponseMessage.Content.ReadAsAsync<IEnumerable<Food>>().Result;
+                foreach (var item in dataObject)
+                {
+                    Console.WriteLine("{0} ",item.Name);
+                    Console.WriteLine("{0} ",item.ID);
+                    Console.WriteLine("{0} ",item.Grade);
+                    Console.WriteLine("{0} ",item.Calories);
+                    Console.WriteLine("{0} ",item.Ingridients);
+                }
+                Console.ReadLine();
+            }
+         else
+         {
+              Console.WriteLine("{0} ({1})", (int)httpResponseMessage.StatusCode, httpResponseMessage.ReasonPhrase);
+         }
+        }
+
+        static public void AddRecordes()
+        {
+            HttpClient client_post = new HttpClient();
+
+            client_post.BaseAddress = new Uri(URL);
+            client_post.DefaultRequestHeaders.Accept.Clear();
+            client_post.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            Food food = new Food
+            {
+                ID = 10,
+                Name = "Shrimps",
+                Calories = 300,
+                Ingridients = "Shrimps, Butter",
+                Grade = 10,
+            };
+
+            var response_post = client_post.PostAsJsonAsync(
+                 "", food).Result;
+
+            Console.WriteLine(response_post);
+        }
+
+    }
+}
---
+++ FoodEntityFW/DAOManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodEntityFW
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Food_entityFramWork
+    {
+        public class DAOManager
+        {
+            public List<Food> Foods()
+            {
+                using (FoodDBEntities FoodEntiites = new FoodDBEntities())
+                {
+                    List<Food> foods = (from c in FoodEntiites.Foods
+                                        select c).ToList();
+                    // List<Food> countriesMS = FoodEntiites.Food.ToList();
+                    return foods;
+                }
+            }
+
+            public void AddToDb(Food f)
+            {
+                using (FoodDBEntities FoodEntiites = new FoodDBEntities())
+                {
+                    FoodEntiites.Foods.Add(f);
+                    FoodEntiites.SaveChanges();
+                }
+            }
+
+            public void UpdateDB(Food food, int id)
+            {
+                using (FoodDBEntities FoodEntiites = new FoodDBEntities())
+                {
+                    Food result = FoodEntiites.Foods.SingleOrDefault(b => b.ID == id);
+                    if (result != null)
+                    {
+                        result.Calories = food.Calories;
+                        result.Grade = food.Grade;
+                        result.ID = food.ID;
+                        result.Ingridients = food.Ingridients;
+                        result.Name = food.Name;
+                        FoodEntiites.SaveChanges();
+                    }
+                }
+            }
+
+            public void RemoveFromDB(int id)
+            {
+                using (FoodDBEntities FoodEntiites = new FoodDBEntities())
+                {
+                    Food result = FoodEntiites.Foods.FirstOrDefault(b => b.ID == id);
+                    if (result != null)
+                        FoodEntiites.Foods.Remove(result);
+                    FoodEntiites.SaveChanges();
+
+                }
+            }
+        }
+    }
+
+}
